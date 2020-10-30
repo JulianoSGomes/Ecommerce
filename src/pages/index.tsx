8aa3c97fce3a69b6f586db3a5c43e824f98b9e64
Juliano Gomes <js.gomes1@hotmail.com>
2020-10-30 05:10:30 -0300

    add globalStyle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,13 @@
 import Head from 'next/head'
-import styles from '../../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Next Template</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>Next Template</h1>
-
-        <p className={styles.description}>template for Nextjs</p>
-      </main>
+      <h1>NEXT TEMPLATE</h1>
     </div>
   )
 }
